19415cf5830cf92e41df9261aaf378caaa996c3d
Ckott <diegoangeles130703@gmail.com>
2024-01-20 18:27:04 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+import "./index.css";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(
+    <App/>
+  )
+
+// react.frament no da para poder ingresar 2 elementos iguales
